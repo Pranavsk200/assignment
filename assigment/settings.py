99a31585ca8be252180ad6bc1993c11b3239d9e4
Pranavsk200 <pranavsk000@gmail.com>
2022-12-15 22:48:26 +0530

    third-commit

--- assigment/settings.py
+++ assigment/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_l5lok(mq4t6pub*cmlhx#(y19lywro=y9tw5^7c%_4=f_=sw$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -129,6 +129,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
